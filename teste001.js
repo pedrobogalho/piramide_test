4bd3f3c66953648f897c22e2e6565da6db2effc7
Pedro Bogalho <pedro.ms.bogalho@gmail.com>
2021-11-12 12:16:33 -0300

    first commit

---
+++ teste001.js
@@ -0,0 +1,10 @@
+// SUBTRACT STRINGS
+function decrease(lines){
+    let n = ''
+    for(let i = 1; i <= lines; i++){
+        n += 'a'
+        if(n.length == 10){
+        console.log(n)}
+      }
+}
+decrease(10)        
